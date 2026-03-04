c6d3ebc79351805fead642ef5c381ff43c58874c
Vla1mer <vla1mer8@gmail.com>
2026-03-04 15:18:11 +0100

    Initial commit

---
+++ internship/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace internship
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Add your DbSet<> properties here when you have entities
+        // public DbSet<YourEntity> YourEntities { get; set; }
+    }
+}
